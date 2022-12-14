732be7619024245d8bdccd257609816b8a7056e0
mantoni <Marton_Antoni@mentor.com>
2022-12-14 09:22:37 +0100

    AOC 2022-14

---
+++ aoc/2022/2022_14.cpp
@@ -0,0 +1,211 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <set>
+#include <tuple>
+#include <functional>
+#include <cstring>
+#include <deque>
+#include <array>
+#include <map>
+#include <random>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstddef>
+#include <cinttypes>
+#include <regex>
+#include <string>
+#include <type_traits>
+#include <variant>
+
+#undef max
+#undef min
+#undef I
+
+#ifdef __GNUC__ 
+#pragma GCC diagnostic ignored "-Wunused-result"
+#endif
+
+#ifdef _DEBUG
+#define ASSERT(x) if(!(x)) __debugbreak()
+#define LOG(...) fprintf(stderr, __VA_ARGS__)
+#define D(expression) expression
+#else
+char* crash_please = (char*)42;
+#define ASSERT(x) 
+//if(!(x)) { printf("%s failed",#x); *crash_please=33; }
+//#define LOG(...) fprintf(stderr, __VA_ARGS__)
+#define LOG(...)
+#define D(expression)
+#endif
+
+#include <chrono>
+
+class cLogPerformance_Guard
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> mStartTime = std::chrono::high_resolution_clock::now();
+    const char* mName;
+public:
+    cLogPerformance_Guard(const char* Name) : mName(Name) {}
+    ~cLogPerformance_Guard()
+    {
+        auto EndTime = std::chrono::high_resolution_clock::now();
+        auto Elapsed = std::chrono::duration_cast<std::chrono::microseconds>(EndTime - mStartTime);
+        LOG("\n--- Elapsed %llu.%llu ms in %s ---\n", (unsigned long long)Elapsed.count() / 1000, (unsigned long long)Elapsed.count() % 1000, mName);
+    }
+    void waypoint()
+    {
+        auto EndTime = std::chrono::high_resolution_clock::now();
+        auto Elapsed = std::chrono::duration_cast<std::chrono::microseconds>(EndTime - mStartTime);
+        LOG("+ %llu.%llu ms: ", (unsigned long long)Elapsed.count() / 1000, (unsigned long long)Elapsed.count() % 1000);
+    }
+} perf_guard("main");
+
+using namespace std;
+using namespace std::chrono;
+using namespace std::string_literals;
+using ull = unsigned long long;
+using ll = long long;
+#define RI(var_name) int var_name; scanf("%d", &var_name);
+#define RIV(var_name, size) vector<int> var_name(size); for(auto &item_of_##var_name: var_name) scanf("%d", &item_of_##var_name);
+#define RII(var_name1, var_name2) int var_name1, var_name2; scanf("%d %d", &var_name1, &var_name2);
+#define RIII(var_name1, var_name2, var_name3) int var_name1, var_name2, var_name3; scanf("%d %d %d", &var_name1, &var_name2, &var_name3);
+#define RIIII(var_name1, var_name2, var_name3, var_name4) int var_name1, var_name2, var_name3, var_name4; scanf("%d %d %d %d", &var_name1, &var_name2, &var_name3, &var_name4);
+#define RL(var_name) ll var_name; scanf("%lld", &var_name);
+#define RLV(var_name, size) vector<ll> var_name(size); for(auto &item_of_##var_name: var_name) scanf("%lld", &item_of_##var_name);
+#define RLUV(var_name, size) vector<ull> var_name(size); for(auto &item_of_##var_name: var_name) scanf("%llu", &item_of_##var_name);
+#define RLL(var_name1, var_name2) ll var_name1, var_name2; scanf("%lld %lld", &var_name1, &var_name2);
+#define RLLL(var_name1, var_name2, var_name3) ll var_name1, var_name2, var_name3; scanf("%lld %lld %lld", &var_name1, &var_name2, &var_name3);
+#define RLLLL(var_name1, var_name2, var_name3, var_name4) ll var_name1, var_name2, var_name3, var_name4; scanf("%lld %lld %lld %lld", &var_name1, &var_name2, &var_name3, &var_name4);
+#define RD(var_name) double var_name; scanf("%lf", &var_name);
+#define RDV(var_name, size) vector<double> var_name(size); for(auto &item_of_##var_name: var_name) scanf("%lf", &item_of_##var_name);
+#define RDD(var_name1, var_name2) double var_name1, var_name2; scanf("%lf %lf", &var_name1, &var_name2);
+#define RDDD(var_name1, var_name2, var_name3) double var_name1, var_name2, var_name3; scanf("%lf %lf %lf", &var_name1, &var_name2, &var_name3);
+#define ALL(cont) cont.begin(), cont.end()
+#define FOR(var, max_value) for(decltype(max_value) var=0;var<max_value;++var)
+
+constexpr ll mod = 1'000'000'007;
+
+#include "aoc_common.h"
+
+char field[1024 * 1024] = { 0 };
+
+char& at(int x, int y)
+{
+    return field[x + y * 1024];
+}
+
+void drawLine(int from_x, int from_y, int to_x, int to_y)
+{
+    int dx = from_x == to_x ? 0 : (from_x > to_x ? -1 : 1);
+    int dy = from_y == to_y ? 0 : (from_y > to_y ? -1 : 1);
+    for (int x = from_x, y = from_y; x != to_x || y != to_y; x += dx, y += dy)
+    {
+        at(x, y) = '#';
+    }
+    at(to_x, to_y) = '#';
+}
+
+bool oneSand(int start_x, int start_y)
+{
+    int x = start_x, y = start_y;
+    auto check=[&](int dx, int dy)
+    {
+        if (at(x + dx, y + dy) == 0)
+        {
+            x += dx;
+            y += dy;
+            return true;
+        }
+        return false;
+    };
+    for (;;)
+    {
+        if (y == 1000)
+            return true;
+        if(check(0, 1))
+            continue;
+        if(check(-1, 1))
+            continue;
+        if(check(1, 1))
+            continue;
+        at(x, y) = '*';
+        return false;
+    }
+}
+
+void prepare()
+{
+}
+
+int read()
+{
+    int bottom = 0;
+    memset(field, 0, 1024 * 1024);
+    cFastFileReader in("in.txt");
+    for (auto line : in)
+    {
+        auto ps = line.toStringVector(", ->", false).ToIntVector();
+        for (int i = 2; i < ps.size(); i += 2)
+        {
+            drawLine(ps[i - 2], ps[i - 1], ps[i], ps[i + 1]);
+            bottom = max(bottom, (int)ps[i - 1]);
+            bottom = max(bottom, (int)ps[i + 1]);
+        }
+    }
+    return bottom;
+}
+
+void Solve()
+{
+    read();
+    int r = 0;
+    while (!oneSand(500, 0))
+        ++r;
+
+    printf("result part 1: %d\n", r);
+}
+
+void Solve2()
+{
+    int floor_y = read() + 2;
+    drawLine(0, floor_y, 1023, floor_y);
+    int r = 0;
+    for (; at(500, 0) == 0;++r)
+    {
+        oneSand(500, 0);
+    }
+
+    printf("result part 2: %d\n", r);
+}
+
+int Init()
+{
+    constexpr int t = 1;
+ //   RI(t);
+    return t;
+}
+
+int main()
+{
+    int t = Init();
+    prepare();
+    for (int tc = 1; tc <= t; ++tc)
+    {
+//         printf("Case #%d: ", tc);
+        Solve();
+        Solve2();
+ //       printf("%s\n", Solve() ? "YES" : "NO");
+//        printf("result: %lld\n", result);
+        //         RI(verdict);
+        //         if (verdict == -1)
+        //             break;
+    }
+}
